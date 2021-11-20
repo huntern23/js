37669c7a9082b9fdb607c12f83fa21daedc0d1a4
Olexandr Strylbitskyi <lil.teern@gmail.com>
2021-11-20 15:29:37 +0200

    hw4 new

--- hw4/main.js
+++ hw4/main.js
@@ -13,32 +13,36 @@
 function rectangleArea(a, b) {
     return a * b;
 }
+console.log(rectangleArea(20,10))
 
 // - створити функцію яка обчислює та повертає площу кола з радіусом r
+
 function areaCircle(r) {
     return Math.pow(r, 2) * 3.14;
 }
+console.log(areaCircle(4))
 
 // - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
 
 function areaCylinder(h, r) {
     return 2 * 3.14 * h * r;
 }
-
+console.log(areaCylinder(10,5))
 // - створити функцію яка приймає масив та виводить кожен його елемент
-
-function arr(arr0, arr1, arr2, arr3, arr4) {
-    let arr = [arr0, arr1, arr2, arr3, arr4];
-    for (let i = 0; i < 5; i++) {
-        console.log(arr[i]);
+let arr=[1,true,'wgegegew', 5,2]
+function arr1(array) {
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i]);
     }
 }
+arr1(arr)
 
 // - створити функцію яка створює параграф з текстом. Текст задати через аргумент
 
 function paragraph(text) {
     document.write(`<p>${text}</p>`);
 }
+paragraph('text')
 
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
 
@@ -49,7 +53,7 @@ function li(text) {
     document.write(`<li>${text}</li>`);
     document.write(`</ul>`);
 }
-
+li('text');
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
 
 function li2(text, count) {
@@ -59,54 +63,26 @@ function li2(text, count) {
     }
     document.write(`</ul>`);
 }
-
+li2('text',4)
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
-
-function arr2(arr0, arr1, arr2, arr3, arr4) {
-    let arr = [arr0, arr1, arr2, arr3, arr4];
+let array = [2,true,'ogoegwog', false, 5]
+function arr2(arr) {
     for (let i = 0; i < 5; i++) {
         document.write(`<ul>`);
         document.write(`<li>${arr[i]}</li>`);
         document.write(`</ul>`);
     }
 }
+arr2(array)
 
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
-
-function arr3(id1, name1, age1, id2, name2, age2, id3, name3, age3) {
-    let arr = [
-        {
-            id: id1,
-            name: name1,
-            age: age1
-        },
-        {
-            id: id2,
-            name: name2,
-            age: age2
-        },
-        {
-            id: id3,
-            name: name3,
-            age: age3
-        }
-    ]
-    document.write(`<div>`)
-    document.write(`${id1} `)
-    document.write(`${name1} `)
-    document.write(`${age1}`)
-    document.write(`</div>`)
-    document.write(`<div>`)
-    document.write(`${id2} `)
-    document.write(`${name2} `)
-    document.write(`${age2}`)
-    document.write(`</div>`)
-    document.write(`<div>`)
-    document.write(`${id3} `)
-    document.write(`${name3} `)
-    document.write(`${age3}`)
-    document.write(`</div>`)
+let array2 = [{id: 1, name: 'Oleg', age: 27}, {id: 2, name: 'Andrii', age: 10}, {id: 3, name: 'Inna', age: 41}];
+function arr3(array) {
+    for (const arrayElement of array) {
+        document.write(`<div>${arrayElement.id} ${arrayElement.name}  ${arrayElement.age}</div>`);
+    }
 }
+arr3(array2)
 
 
 
